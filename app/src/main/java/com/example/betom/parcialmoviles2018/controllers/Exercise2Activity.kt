2a77da2e3d4bff8a6cdd3166f92640d7500acdda
Alberto Luti <betomdq.93@gmail.com>
2018-12-08 12:19:37 -0300

    exercise2 layout finished and on click button methods created with empty body

--- app/src/main/java/com/example/betom/parcialmoviles2018/controllers/Exercise2Activity.kt
+++ app/src/main/java/com/example/betom/parcialmoviles2018/controllers/Exercise2Activity.kt
@@ -2,6 +2,7 @@ package com.example.betom.parcialmoviles2018.controllers
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import com.example.betom.parcialmoviles2018.R
 
 class Exercise2Activity : AppCompatActivity() {
@@ -9,5 +10,13 @@ class Exercise2Activity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_exercise2)
+    }
+
+    fun btnGetContactClicked(view: View) {
+
+    }
+
+    fun btnMultiplicationClicked(view: View) {
+
     }
 }
