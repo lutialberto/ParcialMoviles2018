f0a1de3348aa812b8ecbfe521b0310d0e4bf48a3
Alberto Luti <betomdq.93@gmail.com>
2018-12-08 11:41:42 -0300

    on click method implemented for btnClickeame

--- app/src/main/java/com/example/betom/parcialmoviles2018/controllers/Exercise1Activity.kt
+++ app/src/main/java/com/example/betom/parcialmoviles2018/controllers/Exercise1Activity.kt
@@ -2,12 +2,23 @@ package com.example.betom.parcialmoviles2018.controllers
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Toast
 import com.example.betom.parcialmoviles2018.R
+import com.example.betom.parcialmoviles2018.utilities.LOG_MESSAGE_HELLO_WORLD
+import com.example.betom.parcialmoviles2018.utilities.LOG_TAG_HELLO_WORLD
+import com.example.betom.parcialmoviles2018.utilities.TOAST_EXERCISE_1_ON_CLICK
 
 class Exercise1Activity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_exercise1)
+    }
+
+    fun btnCLickeameClicked(view: View) {
+        Log.d(LOG_TAG_HELLO_WORLD, LOG_MESSAGE_HELLO_WORLD)
+        Toast.makeText(this, TOAST_EXERCISE_1_ON_CLICK,Toast.LENGTH_LONG).show()
     }
 }
---
+++ app/src/main/java/com/example/betom/parcialmoviles2018/utilities/Constants.kt
@@ -0,0 +1,10 @@
+package com.example.betom.parcialmoviles2018.utilities
+
+/*log tags*/
+const val LOG_TAG_HELLO_WORLD = "hola mundo"
+
+/*log messages*/
+const val LOG_MESSAGE_HELLO_WORLD = "El usuario apretó el botón"
+
+/*toast messages*/
+const val TOAST_EXERCISE_1_ON_CLICK = "Hola mundo!"
