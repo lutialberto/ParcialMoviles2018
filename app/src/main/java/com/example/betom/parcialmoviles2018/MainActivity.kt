f627f0cdb9feea7707c36aa50b80a0d1aa6262b2
Alberto Luti <betomdq.93@gmail.com>
2018-12-08 11:14:31 -0300

    minimum layout finished and on click methods ready to implement

--- app/src/main/java/com/example/betom/parcialmoviles2018/MainActivity.kt
+++ app/src/main/java/com/example/betom/parcialmoviles2018/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.betom.parcialmoviles2018
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
 
@@ -9,4 +10,25 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+    fun btnExercise1Click(view : View){
+
+    }
+
+    fun btnExercise2Click(view : View){
+
+    }
+
+    fun btnExercise3Click(view : View){
+
+    }
+
+    fun btnExercise4Click(view : View){
+
+    }
+
+    fun btnExercise5Click(view : View){
+
+    }
+
 }
