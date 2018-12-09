78fbb0658bc7936b4743f41d528307251e9c5f8a
Alberto Luti <betomdq.93@gmail.com>
2018-12-08 21:34:24 -0300

    implemented asyntask en exercise 3

--- app/src/main/java/com/example/betom/parcialmoviles2018/controllers/Exercise3Activity.kt
+++ app/src/main/java/com/example/betom/parcialmoviles2018/controllers/Exercise3Activity.kt
@@ -6,17 +6,41 @@ import android.view.View
 import com.example.betom.parcialmoviles2018.R
 import com.example.betom.parcialmoviles2018.utilities.Prefs
 import kotlinx.android.synthetic.main.activity_exercise3.*
-import java.util.*
+import android.os.AsyncTask
+
+
 
 class Exercise3Activity : AppCompatActivity() {
 
-    enum class TimerState {
+    enum class CounterState {
         Stopped,Paused,Running
     }
 
-    private lateinit var timer: Timer
+    private inner class Counter : AsyncTask<Long, Void, Void>() {
+        override fun doInBackground(vararg period: Long?): Void? {
+            while (true) {
+                publishProgress()
+                if (isCancelled) break
+                try {
+                    period[0]?.let { Thread.sleep(it) }
+                } catch (ie: InterruptedException) {
+                    ie.printStackTrace()
+                }
+                count++
+            }
+            return null
+        }
+
+        override fun onProgressUpdate(vararg values: Void?) {
+            super.onProgressUpdate(*values)
+            updateUI()
+        }
+    }
+
+
+    private lateinit var counter: Counter
     private var count = 0
-    private var timerState = TimerState.Stopped
+    private var counterState = CounterState.Stopped
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -26,57 +50,55 @@ class Exercise3Activity : AppCompatActivity() {
     override fun onResume() {
         super.onResume()
         initTimer()
+        updateUI()
     }
 
-    private fun initTimer() {
-        timerState = Prefs.getTimerState(this)
-        count = if(timerState == TimerState.Stopped){
-            0
-        } else {
-            Prefs.getPreviousTimerCount(this)
-        }
-        if(timerState == TimerState.Running) {
-            startTimer()
-        }
-        updateCountUI()
-    }
-
-    private fun updateCountUI() {
+    private fun updateUI() {
         txtCount.text = count.toString()
     }
 
-    private fun startTimer(){
-        timerState = TimerState.Running
-        timer = object : Timer(){
+    private fun initTimer() {
+        counter = Counter()
+        counterState = Prefs.getCounterState(this)
+        count = if(counterState == CounterState.Stopped){
+            0
+        } else {
+            Prefs.getPreviousCount(this)
         }
+        if(counterState == CounterState.Running) {
+            startTimer()
+        }
+    }
+
+    private fun startTimer(){
+        counterState = CounterState.Running
+        counter.execute(1000L)
     }
 
     override fun onPause() {
         super.onPause()
 
-        if(timerState == TimerState.Running) {
-            timer.cancel()
-        } else if (timer == TimerState.Paused) {
-
+        if(counterState == CounterState.Running) {
+            counter.cancel(true)
         }
 
-        Prefs.setPreviousTimerCount(count,this)
-        Prefs.setTimerState(timerState,this)
+        Prefs.setPreviousCount(count,this)
+        Prefs.setCounterState(counterState,this)
     }
 
     fun btnStartClicked(view: View) {
         startTimer()
-        timerState = TimerState.Running
     }
 
     fun btnStopClicked(view: View) {
-        timer.cancel()
-        timerState = TimerState.Paused
+        counterState = CounterState.Paused
+        counter.cancel(true)
     }
 
     fun btnResetClicked(view: View) {
-        timer.cancel()
-        timerState = TimerState.Stopped
+        counterState = CounterState.Stopped
+        counter.cancel(true)
         count = 0
+        updateUI()
     }
 }
--- app/src/main/java/com/example/betom/parcialmoviles2018/utilities/Prefs.kt
+++ app/src/main/java/com/example/betom/parcialmoviles2018/utilities/Prefs.kt
@@ -1,22 +1,20 @@
 package com.example.betom.parcialmoviles2018.utilities
 
 import android.content.Context
-import android.icu.text.RelativeDateTimeFormatter
 import android.preference.PreferenceManager
 import com.example.betom.parcialmoviles2018.controllers.Exercise3Activity
-import java.util.prefs.Preferences
 
 class Prefs {
     companion object {
 
         private const val PREVIOUS_TIMER_COUNT = "conteo anterior"
 
-        fun getPreviousTimerCount(context: Context): Int {
+        fun getPreviousCount(context: Context): Int {
             val preferences = PreferenceManager.getDefaultSharedPreferences(context)
             return preferences.getInt(PREVIOUS_TIMER_COUNT,0)
         }
 
-        fun setPreviousTimerCount(seconds:Int,context: Context) {
+        fun setPreviousCount(seconds:Int, context: Context) {
             val editor = PreferenceManager.getDefaultSharedPreferences(context).edit()
             editor.putInt(PREVIOUS_TIMER_COUNT,seconds)
             editor.apply()
@@ -24,13 +22,13 @@ class Prefs {
 
         private const val TIMER_STATE_ID = "estado anterior"
 
-        fun getTimerState(context: Context): Exercise3Activity.TimerState {
+        fun getCounterState(context: Context): Exercise3Activity.CounterState {
             val preferences = PreferenceManager.getDefaultSharedPreferences(context)
             val ordinal = preferences.getInt(TIMER_STATE_ID,0)
-            return Exercise3Activity.TimerState.values()[ordinal]
+            return Exercise3Activity.CounterState.values()[ordinal]
         }
 
-        fun setTimerState(state: Exercise3Activity.TimerState,context: Context){
+        fun setCounterState(state: Exercise3Activity.CounterState, context: Context){
             val editor = PreferenceManager.getDefaultSharedPreferences(context).edit()
             val ordinal =state.ordinal
             editor.putInt(TIMER_STATE_ID,ordinal)
